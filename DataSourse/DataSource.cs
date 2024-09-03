34ecb72dd61fb0cbdcef27a720c073d18c4e41e0
Станислав Губарь <Elstagel@gmail.com>
2024-09-03 21:21:29 +0300

    Добавьте файлы проекта.

---
+++ DataSourse/DataSource.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DataSourse
+{
+	internal class DataSource :INotifyPropertyChanged
+	{
+		private readonly Command redCommand;
+		private readonly Command greenCommand;
+		private readonly Command blueCommand;
+		private readonly Command yellowCommand;
+		private string selectedColor = "Black";
+
+		public DataSource()
+		{
+			redCommand = new DelegateCommand(
+				() => SelectedColor = "Red",
+				() => SelectedColor != "Red"
+				);
+			greenCommand = new DelegateCommand(
+				() => SelectedColor = "Green",
+				() => SelectedColor != "Green"
+				);
+			blueCommand = new DelegateCommand(
+				() => SelectedColor = "Blue",
+				() => SelectedColor != "Blue"
+				);
+			yellowCommand = new DelegateCommand(
+				() => SelectedColor = "Yellow",
+				() => SelectedColor != "Yellow"
+				);
+
+
+			PropertyChanged += (sender, e) =>
+			{
+				if (e.PropertyName.Equals(nameof(SelectedColor)))
+				{
+					redCommand.RaiseCanExecuteChanged();
+					greenCommand.RaiseCanExecuteChanged();
+					blueCommand.RaiseCanExecuteChanged();
+					yellowCommand.RaiseCanExecuteChanged();
+				}
+			};
+		}
+
+		public ICommand RedCommand => redCommand;
+		public ICommand GreenCommand => greenCommand;
+		public ICommand BlueCommand => blueCommand;
+		public ICommand YellowCommand => yellowCommand;
+
+		public string SelectedColor
+		{
+			get => selectedColor;
+			set
+			{
+				if (selectedColor != value)
+				{
+					selectedColor = value;
+					OnPropertyChanged(new PropertyChangedEventArgs(nameof(SelectedColor)));
+				}
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void OnPropertyChanged(PropertyChangedEventArgs e)
+		{
+			PropertyChanged?.Invoke(this, e);
+		}
+	}
+}
+
---
+++ DataSourse/DelegateCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSourse
+{
+	internal sealed class DelegateCommand : Command
+	{
+		private static readonly Func<bool> defaultCanExecuteMethod = () => true;
+		private readonly Func<bool> canExecuteMethod;
+		private readonly Action executeMethod;
+
+		public DelegateCommand(Action executeMethod) :
+			this(executeMethod, defaultCanExecuteMethod)
+		{
+		}
+
+		public DelegateCommand(Action executeMethod, Func<bool> canExecuteMethod)
+		{
+			this.executeMethod = executeMethod;
+			this.canExecuteMethod = canExecuteMethod;
+		}
+
+		protected override bool CanExecute()
+		{
+			return canExecuteMethod();
+		}
+
+		protected override void Execute()
+		{
+			executeMethod();
+		}
+	}
+}
